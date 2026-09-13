ab95a1ffd14b8a2c26bff15a141fc48c84fcb54b
z.o_ <wowvww@gmail.com>
2026-09-13 21:45:38 +0300

    Change Supabase_ANON_KEY in config.js
    Updated the Supabase anonymous key in the configuration.

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 
 const CONFIG = {
   SUPABASE_URL: "https://hodsxnjmgpbuegbazens.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_clKEcihEGUxTDJWK_dq8hg_sKQZEv6H",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvZHN4bmptZ3BidWVnYmF6ZW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5OTA0NDgsImV4cCI6MjEwNDU2NjQ0OH0.n0c3vUSZrLsa_DdFgfFB5VJAIelgeS8yheylScXEKTg",
   TMDB_API_KEY: "6c4b1dd7cbbb4ae5a0bf79484e428b8e"
 };
